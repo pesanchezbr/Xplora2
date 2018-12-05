7b6f7f7b812c00b255c86effbc75991433470a17
Pedro Sanchez Brandt <psanchez039@gmail.com>
2018-12-05 11:15:06 -0400

    tambien este

--- ControllerBoard.cs
+++ ControllerBoard.cs
@@ -19,11 +19,8 @@ public class ControllerBoard : MonoBehaviour {
 
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     void paquitaBarrio(){
         print("Maldita Lisiada");
-=======
     //vivavv
     void OlaKAse(){
 
@@ -32,15 +29,10 @@ public class ControllerBoard : MonoBehaviour {
       print("Hace");
     }
 
-
->>>>>>> 996c8817e425af6425925fe03baa1bc6b5402538
-=======
     /* aarb */
     public void AddDestino(int comp, string destino,bool val, string tipo){
         //Agregar destino al granjero que sale de un componente
         destinos[comp].Add(destino);
         destinosComp[comp].Add(new DestinoComponentes(destino,val,tipo));
->>>>>>> 42240b4bd3679b17ee3497db7d4cef962192f747
     }
->>>>>>> 42240b4bd3679b17ee3497db7d4cef962192f747
 }
